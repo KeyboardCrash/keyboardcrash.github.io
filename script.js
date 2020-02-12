353246518c8266331f2cabd4ad8755c8a54cfc13
brandon.lu1 <brandon.lu1@ucalgary.ca>
2020-02-11 17:13:17 -0700

    Changed display to input val
    a

--- script.js
+++ script.js
@@ -3,35 +3,40 @@ $(document).on("click", ".btn", function() {
     let x = $('#calculator #display').html();
     let y = $(this).html();
     if (x === "ERROR") {
-        $('#calculator #display').html(y);
+        $('#calculator #display').val(y);
     } else {
         if (x.length >= 15) {
-            $('#calculator #display').html(x.slice(1, x.length) + y);
+            $('#calculator #display').val(x.slice(1, x.length) + y);
 
         } else {
-            $('#calculator #display').html($('#calculator #display').html() + y);
+            $('#calculator #display').val($('#calculator #display').val() + y);
         }
     }
 })
 
 $(document).on("click", "#math", function() {
-    let x = $('#calculator #display').html();
+    let x = $('#calculator #display').val();
     try {
-        $('#calculator #display').html(eval(x));
-        $('#calculator #history').html(x);
+        $('#calculator #display').val(eval(x));
+        $('#calculator #history').html(x + "=");
 
     } catch {
-        $('#calculator #display').html("ERROR");
+        $('#calculator #display').val("ERROR");
     }
 })
 
 $(document).on("click", "#clear", function() {
-    $('#calculator #display').html("");
+    $('#calculator #display').val("");
 })
 
+/*
+<td colspan=5><input type="text" id="display" minlength="0" maxlength="50" style="width: 100%; height: 100%;"></td>
+*/
+
+
 $(document).on("click", "#back", function() {
-    let x = $('#calculator #display').html();
-    $('#calculator #display').html(x.slice(0, x.length - 1));
+    let x = $('#calculator #display').val();
+    $('#calculator #display').val(x.slice(0, x.length - 1));
 })
 
 /*
